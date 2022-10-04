fab2ece9b48fb3b3a8c54acddb5506286f91b800
phcsilva056 <phc.silva@outlook.com.br>
2022-10-04 18:15:35 -0300

    fix/atualizando models

---
+++ backend/src/business/OrderBusiness.ts
@@ -0,0 +1,9 @@
+import { OrderDatabase } from "../database/OrderDatabase";
+
+export class OrderBusiness {
+  constructor(private orderDatabase: OrderDatabase) {}
+
+  public addProduct = async (input:any) => {
+    
+  };
+}
